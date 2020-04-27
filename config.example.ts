648182367aa375fd69cc2b963e1c7f1183d467cb
Rebel <johnhartigan@ya.ru>
2020-04-28 02:55:16 +0300

    add config example

---
+++ config.example.ts
@@ -0,0 +1,10 @@
+export const server = {
+  host: 'localhost',
+  port: 3000,
+};
+export const ams = {
+  appsPath: './applications',
+  host: 'http://localhost:1111',
+  user: 'admin',
+  password: '123456789',
+};
--- ffmpeg.bat
+++
@@ -1,1 +0,0 @@
-d:\install\ffmpeg\ffmpeg.exe -re -f lavfi -i color=c=black:s=1280x720:r=25/1 -c:v h264 -t 60 -f flv "rtmp://localhost/live/channel?password=password"
