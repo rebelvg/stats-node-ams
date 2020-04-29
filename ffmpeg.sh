8d558b90e89df69042b45ad609c0a637661de26f
Rebel <johnhartigan@ya.ru>
2020-04-30 00:41:16 +0300

    update ffmpeg script

--- ffmpeg.sh
+++ ffmpeg.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-"/d/install/ffmpeg/ffmpeg.exe" -re -f lavfi -i color=c=black:s=1280x720:r=25/1 -c:v h264 -t 60 -f flv "rtmp://localhost/live/channel?password=password"
+"/d/install/ffmpeg/ffmpeg.exe" -re -f lavfi -i color=c=red:s=1280x720:r=25/1 -c:v h264 -t 60 -f flv "rtmp://localhost/live/channel?password=password"
