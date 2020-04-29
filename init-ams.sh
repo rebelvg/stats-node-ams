6e4b15e356ae238e00d531705684667bfc29504d
Rebel <johnhartigan@ya.ru>
2020-04-29 23:48:40 +0300

    switch to lr new line char

--- init-ams.sh
+++ init-ams.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
+
 cp -a /opt/adobe/ams/samples/videoPlayer /opt/adobe/ams/webroot
 
-# sed -i 's/<Allow>ping<\/Allow>/<Allow>All<\/Allow>/g' /opt/adobe/ams/conf/Users.xml
-# sed -i 's/<Deny>All<\/Deny>/<Deny><\/Deny>/g' /opt/adobe/ams/conf/Users.xml
+sed -i 's/<Allow>ping<\/Allow>/<Allow>All<\/Allow>/g' /opt/adobe/ams/conf/Users.xml
+sed -i 's/<Deny>All<\/Deny>/<Deny><\/Deny>/g' /opt/adobe/ams/conf/Users.xml
