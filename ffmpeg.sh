e6b76c6be8e2d35e1da4a65eecb6fb7480add0bd
Volodymyr Rebel <johnhartigan@ya.ru>
2021-01-10 17:21:08 +0200

    fix path

--- ffmpeg.sh
+++ ffmpeg.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-"/d/install/ffmpeg/ffmpeg.exe" -re -f lavfi -i color=c=red:s=1280x720:r=25/1 -c:v h264 -t 60 -f flv "rtmp://localhost/live/channel?password=password"
+
+"ffmpeg" -re -f lavfi -i color=c=red:s=1280x720:r=25/1 -c:v h264 -t 60 -f flv "rtmp://localhost/live/channel?password=password"
